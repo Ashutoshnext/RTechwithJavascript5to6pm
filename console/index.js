08574ee0f5e44fafef67e7d2ac8c64f43ac91c67
Ashutosh Kumar <ashutoshpal911097@gmail.com>
2026-03-30 18:05:10 +0530

    'console'

---
+++ console/index.js
@@ -0,0 +1,14 @@
+console.log('hello world');
+
+console.count()
+console.count()
+console.count()
+console.count()
+console.count()
+
+console.warn('hell world')
+console.error('hell world')
+
+let arr=['city','bihar','india'];
+
+console.table(arr)
