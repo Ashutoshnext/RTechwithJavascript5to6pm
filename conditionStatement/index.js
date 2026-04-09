a2540d271ab4b2ef44f1c6920a2dcf8f9d3f2b1d
Ashutosh Kumar <ashutoshpal911097@gmail.com>
2026-04-09 19:54:34 +0530

    switch

---
+++ conditionStatement/index.js
@@ -0,0 +1,25 @@
+// let age=18;
+// if (age>18) {
+// //true statement
+// console.log('Adult');
+
+// } else {
+//     //false statement
+//     console.log('Minnor');
+    
+
+// }
+
+
+let marks=110;
+44
+if(marks>=80){
+    console.log('Grad A');
+    
+}else if(marks<=79 && marks>=60){
+    console.log('Grad B');
+    
+}else{
+    console.log('Fail');
+    
+}
---
+++ conditionStatement/switch.js
@@ -0,0 +1,27 @@
+let day='Monday';
+switch (day) {
+    case 'Monday':
+        console.log('Week of first day');
+        break;
+    case 'Tuesday':
+        console.log('Week of Second day');
+        break;
+    case 'Wednesday':
+        console.log('Week of third day');
+        break;
+    case 'Thrusday':
+        console.log('Week of fourth day');
+        break;
+    case 'Friday':
+        console.log('Week of fifth day');
+        break;
+    case 'Saturday':
+        console.log('Week of first day');
+        break;
+    case 'Sunday':
+        console.log('Week of Last day');
+        break; 
+    default:
+        console.log('Please Insert valid value');
+        break;
+}
