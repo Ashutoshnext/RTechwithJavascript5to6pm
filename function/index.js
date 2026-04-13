8eedac467c19c164f80a630f4c8212df9afc06e9
Ashutosh Kumar <ashutoshpal911097@gmail.com>
2026-04-13 17:20:50 +0530

    function

--- function/index.js
+++ function/index.js
@@ -2,6 +2,8 @@
 //     console.log('Hello');
 // }
 
+
+
 // greet()
 
 
@@ -29,10 +31,75 @@
 // greet('Raju')
 
 
-function greet(firstname='Ram',lastname){
-    lastname=lastname|| 'kumar' //
-    console.log(`my name is ${firstname} ${lastname}`);
+
+// function greet(firstname='Ram',lastname){
+//     lastname=lastname|| 'kumar' //
+//     console.log(`my name is ${firstname} ${lastname}`);
+// }
+// greet('Raju','Ram')
+// greet('Sita','Devi')
+// greet()
+
+
+
+// function expression
+
+// let greet=function(){
+// console.log('hello world');
+
+// }
+// greet()
+
+// anonymous function
+
+// (
+//     function(){
+//         console.log('hello world');
+        
+//     }
+// ) ()
+
+
+
+// callback function
+// function greet(name,callback){
+// console.log('my name is ',name);
+// callback()
+// }
+
+// greet('Raju',sumit)
+
+
+// function sumit(){
+//     console.log('hello sumit');
+// }
+
+
+// arrow function 
+
+// const greet=()=>{
+// console.log('hello world!');
+
+// }
+// greet()
+
+// const greet=()=>console.log('hello');
+// greet()
+
+
+function sum(a,b){
+    //do
+    // console.log(a+b);
+    
+    return a+b
+    
+
+
+    //return
+
 }
-greet('Raju','Ram')
-greet('Sita','Devi')
-greet()
+// console.log(sum(5,8));
+let result= sum(5,8)
+console.log(result);
+
+
